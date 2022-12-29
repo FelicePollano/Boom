65c3b6fcdc6197bdc0a2405c5282d6681d5c96b0
Felice Pollano <felice@felicepollano.com>
2022-12-29 15:03:01 +0100

    added weapon

--- Weapons/Flash.gd
+++ Weapons/Flash.gd
@@ -1,17 +1,20 @@
 extends Spatial
 
 
-var ak47 = preload("res://Weapons/Ak47/Ak47.tscn")
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# load default weapon
-	$KinematicBody.add_child(ak47.instance())
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	pass
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Timer_timeout() -> void:
+	queue_free()
