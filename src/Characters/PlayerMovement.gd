1c6caf9769c73a436260c4c7a3e72d7779a7f42f
Felice Pollano <felice@felicepollano.com>
2022-12-31 13:40:20 +0100

    add victory

--- src/Characters/PlayerMovement.gd
+++ src/Characters/PlayerMovement.gd
@@ -4,7 +4,7 @@ var ak47 = preload("res://Weapons/Ak47/Ak47.tscn")
 var flash = preload("res://Weapons/Flash.tscn")
 onready var ouch : AudioStreamPlayer = $Ouch
 onready var raycast =$RayCast
-onready var health_label : Label = $CanvasLayer/Control/Label2
+onready var health_label : Label = $CanvasLayer/Control/ColorRect/Label2
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
@@ -43,6 +43,10 @@ func on_fire()->void:
 func damage(amount)->void:
 	health-=amount
 	health_label.text = "%s"%health
+	if health <= 50:
+		health_label.add_color_override("font_color",Color.darkred)
+	else:
+		health_label.add_color_override("font_color",Color.white)
 	var f=flash.instance()
 	f.color=Color.darkred
 	add_child(f)
--- src/Levels/Level0.gd
+++ src/Levels/Level0.gd
@@ -1,9 +1,20 @@
 extends LevelBase
 
 var how_many=3
-
+var victory=false
 
 func _on_Timer_timeout() -> void:
 	if how_many > 0:
 		how_many-=1
 		spawn_enemies()
+
+func _process(delta: float) -> void:
+	._process(delta)
+	var dead=0
+	for enemy in get_tree().get_nodes_in_group("Enemy"):
+		if enemy.health<=0:
+			dead+=1
+	if dead>=9 and not victory: # 3*3 enemies in this level
+		victory=true
+		$AudioStreamPlayer.stream=load("res://Music/boom_quiet.mp3")
+		$AudioStreamPlayer.play()
